e7b4d780057067e9bc36f71efd1d9d55bdbc72b5
Rahul Kumar <raulk023@gmail.com>
2024-06-19 00:34:29 +0530

    change in resume url

--- data/socials.ts
+++ data/socials.ts
@@ -5,5 +5,5 @@ export const socials: Socials = {
   linkedin: "https://www.linkedin.com/in/rahul-kumar23/",
   mail: "mailto:raulk023@gmail.com",
   resume:
-    "https://drive.google.com/file/d/184ZR4-5lA94xmi7BxA8e445uNo1eQ3Rf/view?usp=drive_link",
+    "https://drive.google.com/file/d/1gfr90KaSeMpZufYviNuAvrOH2do1xRs5/view?usp=drive_link",
 };
